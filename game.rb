52a9a92721c5151ecaae0cf03c8f2ae7624013e8
Adam Hyland <A.D.Hyland@2009.ljmu.ac.uk>
2012-10-23 13:10:45 +0100

    last week stuff

---
+++ game.rb
@@ -0,0 +1,14 @@
+
+player1 = "aaron"
+health1 = "60"
+
+player2 = "Molvak"
+player3 = "Syntha"
+
+
+
+
+puts "#{player1}'s health is #{health1}"
+
+puts"\nPlayers:\n \t#{player1}\n \t#{player2}\n \t#{player3}"
+
